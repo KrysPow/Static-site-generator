802ba82716c4b6a80924e08c4ff3f8acdbca843d
KrysPow <pauer.christoph@gmail.com>
2024-04-14 23:46:17 +0200

    added split_nodes function and all tests for it

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +20,54 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
 
+    def test_split_nodes_code(self):
+        node = TextNode('This text has ´code´ in it', 'text')
+        self.assertEqual(split_nodes_delimiter([node], '´', 'code'), [TextNode('This text has ', 'text', None), TextNode('code', 'code', None), TextNode(' in it', 'text', None)])
+
+    def test_split_nodes_bold(self):
+        node = TextNode('This text has **bold text** in it', 'text')
+        self.assertEqual(split_nodes_delimiter([node], '**', 'bold'), [TextNode('This text has ', 'text', None), TextNode('bold text', 'bold', None), TextNode(' in it', 'text', None)])
+    
+    def test_split_nodes_italic(self):
+        node = TextNode('This text has *italic* in it', 'text')
+        self.assertEqual(split_nodes_delimiter([node], '*', 'italic'),[TextNode('This text has ', 'text', None), TextNode('italic', 'italic', None), TextNode(' in it', 'text', None)])
+
+    def test_split_nodes_pure_text(self):
+        node = TextNode('This is text', 'text')
+        self.assertEqual(split_nodes_delimiter([node], '**', 'bold'), [TextNode('This is text', 'text', None)])
+
+    def test_split_nodes_co_bo_it(self):
+        node = TextNode('This text has ´code´ in it, additionally **bold text** and *italic* text.', 'text')
+        output = split_nodes_delimiter(
+                    split_nodes_delimiter(
+                        split_nodes_delimiter([node], '´', 'code'), '**', 'bold'), '*', 'italic')
+        self.assertEqual(output, [TextNode('This text has ', 'text', None), TextNode('code', 'code', None), TextNode(' in it, additionally ', 'text', None), TextNode('bold text', 'bold', None), TextNode(' and ', 'text', None), TextNode('italic', 'italic', None), TextNode(' text.', 'text', None)])
+
+    def test_split_nodes_multiple_code(self):
+        node =TextNode('This is ´code1´and this is ´code2´, also there is some **bold** and *italic* thrown *in*, **haha**', 'text')
+        output = split_nodes_delimiter(
+                    split_nodes_delimiter(
+                        split_nodes_delimiter([node], '´', 'code'), '**', 'bold'), '*', 'italic')
+        self.assertEqual(output, [TextNode('This is ', 'text', None),
+                                    TextNode('code1', 'code', None),
+                                    TextNode('and this is ', 'text', None),
+                                    TextNode('code2', 'code', None),
+                                    TextNode(', also there is some ', 'text', None),
+                                    TextNode('bold', 'bold', None), 
+                                    TextNode(' and ', 'text', None),
+                                    TextNode('italic', 'italic', None),
+                                    TextNode(' thrown ', 'text', None),
+                                    TextNode('in', 'italic', None),
+                                    TextNode(', ', 'text', None), 
+                                    TextNode('haha', 'bold', None), 
+                                    TextNode('', 'text', None)])
+        
+    def test_split_nodes_markdown_syntax_error(self):
+        node = TextNode('This is *wrong.', 'text')
+        print(split_nodes_delimiter([node], '*', 'italic'))
+        #self.assertEqual(split_nodes_delimiter([node], '*', 'italic'), Exception('Invalid markdown syntax. Use * twice'))
 
 if __name__ == "__main__":
     unittest.main()
+
+
--- src/textnode.py
+++ src/textnode.py
@@ -11,3 +11,22 @@ class TextNode():
     
     def __repr__(self):
         return f'TextNode{self.text, self.text_type, self.url}'
+    
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_list = []
+    for oldnode in old_nodes:
+        if oldnode.text_type != 'text':
+            new_list.append(oldnode)
+        else:
+            if delimiter in oldnode.text:
+                on_split = oldnode.text.split(delimiter)
+                if len(on_split)%2 == 1 and len(on_split) > 1:
+                    nw_nodes = [TextNode(on_split[i], 'text') if i%2 == 0 else TextNode(on_split[i], text_type) for i in range(len(on_split))]
+                    new_list.extend(nw_nodes)
+                else:
+                    raise Exception(f'Invalid markdown syntax. Use {delimiter} twice')
+            else:
+                new_list.extend([oldnode])
+    return new_list
+    
